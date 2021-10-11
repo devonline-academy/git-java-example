538d601647e209221cdad7d1dc1a60125b91a83f
devonline <devonline.academy@gmail.com>
2021-10-11 11:36:00 +0300

    Implement `makeUserMove` method

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,6 +16,7 @@
  */
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * @author devonline
@@ -80,7 +81,40 @@ public class TicTacToe {
     }
 
     private static void makeUserMove(char[][] gameTable) {
+        while (true) {
+            System.out.println("Please type number between 1 and 9:");
+            String string = new Scanner(System.in).nextLine();
+            if (string.length() == 1) {
+                char digit = string.charAt(0);
+                if (digit >= '1' && digit <= '9') {
+                    if (makeUserMoveToCell(gameTable, digit)) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
 
+    private static boolean makeUserMoveToCell(char[][] gameTable, char digit) {
+        char[][] mappingTable = {
+                {'7', '8', '9'},
+                {'4', '5', '6'},
+                {'1', '2', '3'}
+        };
+        for (int i = 0; i < mappingTable.length; i++) {
+            for (int j = 0; j < mappingTable[i].length; j++) {
+                if (mappingTable[i][j] == digit) {
+                    if (gameTable[i][j] == ' ') {
+                        gameTable[i][j] = 'X';
+                        return true;
+                    } else {
+                        System.out.println("Can't make a move, because the cell is not free! Try again!");
+                        return false;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     private static void makeComputerMove(char[][] gameTable) {

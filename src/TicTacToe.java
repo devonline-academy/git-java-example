817837ae228d4c0591bb385e8210655987619ed0
devonline <devonline.academy@gmail.com>
2021-10-11 11:25:30 +0300

    Decompose game on separate empty methods

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -15,12 +15,70 @@
  *
  */
 
+import java.util.Random;
+
 /**
  * @author devonline
  * @link http://devonline.academy/java-basic
  */
 public class TicTacToe {
     public static void main(String[] args) {
+        System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9:");
+        printTableMapping();
+        if (new Random().nextBoolean()) {
+            makeComputerMove();
+            printGameTable();
+        }
+        while (true) {
+            makeUserMove();
+            printGameTable();
+            if (isUserWin()) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            makeComputerMove();
+            printGameTable();
+            if (isComputerWin()) {
+                System.out.println("COMPUTER WIN!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!");
+    }
 
+    private static void printTableMapping() {
+
+    }
+
+    private static void printGameTable() {
+
+    }
+
+    private static void makeUserMove() {
+
+    }
+
+    private static void makeComputerMove() {
+
+    }
+
+    private static boolean isUserWin() {
+        return false;
+    }
+
+    private static boolean isComputerWin() {
+        return false;
+    }
+
+    private static boolean isDraw() {
+        return false;
     }
 }
